9f83733beb2df434fdfca3ac8991ff88e963e46c
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 16:48:20 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    loader: 'custom',
-    path: 'https://www.notion.so',
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    loader: 'custom',
+    path: '',
+    customLoader: ({ src, width, quality }) => {
+      return `${src}?w=${width}&q=${quality || 75}`
+    },
   },
 }
