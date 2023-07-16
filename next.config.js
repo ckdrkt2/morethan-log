18365b435e104a1c30a1e505cd7e6d9324f98d37
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 16:57:42 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
-    loader: 'custom',
+    loader: 'imgix',
     path: '',
-    customLoader: ({ src, width, quality }) => {
-      if (src.startsWith('http')) {
-        // 외부 URL에 대한 처리
-        return `${src}?w=${width}&q=${quality || 75}`;
-      } else {
-        // 프로젝트 내부의 URL에 대한 처리
-        return `/path/to/your/local/image/directory${src}?w=${width}&q=${quality || 75}`;
-      }
-    },
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
