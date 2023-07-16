fab82247ba891fe64864e4a325228ba691658128
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 16:42:39 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    loader: 'imgix',
-    path: 'https://og-image-korean.vercel.app',
+    loader: 'custom',
+    path: 'https://www.notion.so',
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
