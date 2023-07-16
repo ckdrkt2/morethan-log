8964d7a9f83937d71e1cfb7448ff746f81feecfc
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 17:59:00 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
     loader: 'imgix',  // or 'imgix', 'cloudinary'
-    path: '/',  // replace with the base path of your images on Notion
+    path: 'https://file.notion.so',  // replace with the base path of your images on Notion
   },
 }
