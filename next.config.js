2af7c3f1559af0a9cca928254ee82816ae6e92c4
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 16:51:20 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,13 @@ module.exports = {
     loader: 'custom',
     path: '',
     customLoader: ({ src, width, quality }) => {
-      return `${src}?w=${width}&q=${quality || 75}`
+      if (src.startsWith('http')) {
+        // 외부 URL에 대한 처리
+        return `${src}?w=${width}&q=${quality || 75}`;
+      } else {
+        // 프로젝트 내부의 URL에 대한 처리
+        return `/path/to/your/local/image/directory${src}?w=${width}&q=${quality || 75}`;
+      }
     },
   },
 }
