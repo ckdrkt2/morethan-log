473449de37cbbafe3edebaf4c689c903080aa44b
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 16:30:27 +0000

    add image loader

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    loader: 'akamai',
-    path: '/',
+    loader: 'imgix',
+    path: 'https://og-image-korean.vercel.app',
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
 }
