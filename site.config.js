1dbfd76c3b3bb35810718dcdc0497286ca4d6ba6
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 14:41:38 +0000

    github action

--- site.config.js
+++ site.config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Changshin Yoo",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "MLOps Engineer",
+    bio: "Hi",
+    email: "bbiuyy@gmail.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "ckdrkt2",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/ckdrkt2/morethan-log",
     },
   ],
   // blog setting (required)
@@ -24,7 +24,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://ckdrkt2.github.io",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
