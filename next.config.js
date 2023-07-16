18ab2d7fd57b1345fdfa05e245d973f2943392c2
ckdrkt2 <bbiuyy@gmail.com>
2023-07-16 18:11:38 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    // domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
     loader: 'imgix',  // or 'imgix', 'cloudinary'
-    path: 'https://file.notion.so/f/s',  // replace with the base path of your images on Notion
+    path: 'https://www.notion.so',  // replace with the base path of your images on Notion
   },
 }
